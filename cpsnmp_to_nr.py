25d99b8b1ede5d09c325de91a1f5a3fc90f1da20
Matt A <mambroziak@users.noreply.github.com>
2019-09-09 14:30:49 -0400

    refactored for JSON file

--- cpsnmp_to_nr.py
+++ cpsnmp_to_nr.py
@@ -3,7 +3,28 @@
 # *******************************************************************************
 # Name: cpsnmp_to_nr.py
 # Description: An automation script for Check Point Security Gateways to gather 
-#  metrics via SNMP and send an aggregated payload to New Relic Insights
+#  metrics via SNMP and send an aggregated payload to New Relic Insights. 
+#
+# Example metrics_list.json :
+#[ 
+#   {
+#      "metric_name":"memoryMetric",
+#      "metric_oid":"1.6.7.8",
+#   },
+#   {
+#      "metric_name":"fwMetric",
+#      "metric_oid":"1.9.8.%.0",
+#      "index_oid":"1.2.3.0", 
+#      "labels":[
+#         {
+#            "label_name":"interface_id",
+#            "label_oid":"1.3.4.%.0"
+#         }
+#      ]
+#   }, ...
+#]
+#
+# PIP Requirements: pip install requests pysnmp
 #
 # Copywrite 2019, Check Point Software
 # www.checkpoint.com
@@ -75,18 +96,6 @@ def cast(value):
                 pass
     return value
 
-def get_bulk(target, oids, credentials, count, start_from=0, port=161,
-             engine=hlapi.SnmpEngine(), context=hlapi.ContextData()):
-    handler = hlapi.bulkCmd(
-        engine,
-        credentials,
-        hlapi.UdpTransportTarget((target, port)),
-        context,
-        start_from, count,
-        *construct_object_types(oids)
-    )
-    return fetch(handler, count)
-
 def send_to_nr(events):
     nr_url = 'https://insights-collector.newrelic.com/v1/accounts/%s/events' % OPTIONS.nr_account_num
  
@@ -98,7 +107,6 @@ def send_to_nr(events):
     try:
         resp = requests.post(nr_url, json=events, headers=headers) 
         resp.raise_for_status()
-        #print('')
     except HTTPError as http_err:
         print('HTTP error occurred: %s' % http_err) 
     except Exception as err:
@@ -146,40 +154,34 @@ def main(argv=None):
         parser.print_help()
         os._exit(1)
     
-    # Pre-processing of OIDs
-    #cp_proc_usage_oids = get_cp_proc_usage_oids() # Get CPU Count to generate Proc Usage OIDs
-    
+  
     # read metrics_list.json file
-    #with open('metrics_list.json', 'r') as jsonfile:
-    #    filedata = jsonfile.read()
+    jsonfilename = 'metrics_list.json'
+    print('Reading %s...' % jsonfilename )
+    with open(jsonfilename, 'r') as jsonfile:
+        filedata = jsonfile.read()
 
     #load metrics_list from file data
-    #metrics_list = json.loads(filedata)
-    metrics_list = [
-      {'metric_name': 'cpvIpsecEspEncPkts', 'metric_oid': '1.3.6.1.4.1.2620.1.2.5.4.5'},
-      {'metric_name': 'cpvIpsecEspDecPkts', 'metric_oid': '1.3.6.1.4.1.2620.1.2.5.4.6'},
-      {'metric_name': 'memTotalReal64', 'metric_oid': '1.3.6.1.4.1.2620.1.6.7.4.3.0'},
-      {'metric_name': 'memActiveReal64', 'metric_oid': '1.3.6.1.4.1.2620.1.6.7.4.4.0'},
-      {'metric_name': 'memFreeReal64', 'metric_oid': '1.3.6.1.4.1.2620.1.6.7.4.5.0'}, 
-      {'metric_name': 'multiProcUsage', 'metric_oid': '1.3.6.1.4.1.2620.1.6.7.5.1.5.%.0', 'index_oid': '1.3.6.1.4.1.2620.1.6.7.2.7.0', 'labels': [{'label_name': 'multiProcIndex', 'label_oid': '1.3.6.1.4.1.2620.1.6.7.5.1.1.%.0'}]},
-      {'metric_name': 'ifInOctets', 'metric_oid': '1.3.6.1.2.1.2.2.1.10.%', 'index_oid': '1.3.6.1.2.1.2.1.0', 'labels': [{'label_name': 'ifDescr', 'label_oid': '1.3.6.1.2.1.2.2.1.2.%'}]}
-     ]
+    metrics_list = json.loads(filedata)
+#    metrics_list = [
+#      {'metric_name': 'memFreeReal64', 'metric_oid': '1.3.6.1.4.1.2620.1.6.7.4.5.0'}, 
+#      {'metric_name': 'ifInOctets', 'metric_oid': '1.3.6.1.2.1.2.2.1.10.%', 'index_oid': '1.3.6.1.2.1.2.1.0', 'labels': [{'label_name': 'ifDescr', 'label_oid': '1.3.6.1.2.1.2.2.1.2.%'}]},
+#     ]
     
     nr_payload = []
     # Query SNMP and build events payload
 
     for item in metrics_list:
-        elements = ''
+        count = ''
         labels = {}
-        if 'index_oid' in item:
-            elements = get_snmp(OPTIONS.snmp_agent_ip, [item['index_oid']], hlapi.CommunityData(OPTIONS.snmp_community))
-            print('Element count: %s' % elements[item['index_oid']])
-        
-        if elements:
-            for i in range(1, (elements[elements.keys()[0]] + 1)): #elements[item['index_oid']]
+        if 'index_oid' in item: 
+            count = get_snmp(OPTIONS.snmp_agent_ip, [item['index_oid']], hlapi.CommunityData(OPTIONS.snmp_community)) # integer expected
+            print('Count for index OID %s: %s' % (item['index_oid'], count[item['index_oid']]))
+
+            for i in range(1, (count[count.keys()[0]] + 1)): 
                 current_metric_oid = item['metric_oid'].replace('%', str(i))
                 metric_data = get_snmp(OPTIONS.snmp_agent_ip, [current_metric_oid], hlapi.CommunityData(OPTIONS.snmp_community))
-                print(metric_data)
+                #print(metric_data)
                 if metric_data[metric_data.keys()[0]]:
                     if 'labels' in item.keys():
                         for label in item['labels']:
@@ -188,14 +190,16 @@ def main(argv=None):
                             labels = {label['label_name']: label_data[label_data.keys()[0]]} 
                             #print(labels)
                     nr_payload.append({'eventType': 'CheckPointPerformance', 'metricType': 'Performance', item['metric_name']: metric_data[metric_data.keys()[0]], 'labels': labels})
+                else:
+                    print('  Value is empty or not found for OID: %s' % (current_metric_oid))
         else:
             current_metric_oid = item['metric_oid']
             metric_data = get_snmp(OPTIONS.snmp_agent_ip, [current_metric_oid], hlapi.CommunityData(OPTIONS.snmp_community))
-            print(metric_data)
+            #print(metric_data)
             if metric_data[metric_data.keys()[0]]:
                 nr_payload.append({'eventType': 'CheckPointPerformance', 'metricType': 'Performance', item['metric_name']: metric_data[metric_data.keys()[0]], 'labels': labels})
             else:
-                print('\tOID data is null: %s' % (current_metric_oid))
+                print('  Value is empty or not found for OID: %s' % (current_metric_oid))
     
     # Send all events to New Relic
     if nr_payload:
